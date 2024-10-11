26680667651f6fffc23199590df7fabfc3e8f7fa
pooyazangiabadi <137812245+pooyazangiabadi@users.noreply.github.com>
2024-10-11 16:15:19 +0330

    CountingSort

---
+++ src/main/java/sorting/counting/CountingSort.java
@@ -0,0 +1,32 @@
+package sorting.counting;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] sorted=countingSort(new int[]{3,2,1,1,1,3,2,2});
+        System.out.println(Arrays.toString(sorted));
+    }
+
+    private static int[] countingSort(int[] arr){
+        int max = Arrays.stream(arr).max().getAsInt();
+
+        int[] countingArray = new int[max+1];
+
+        for (int i : arr) {
+            countingArray[i] +=1;
+        }
+
+        int[] outputArray=new int[arr.length];
+
+        int index=0;
+        for (int i = 0; i < max+1; i++) {
+            for (int j=0;j < countingArray[i]; j++){
+                outputArray[index]=i;
+                index++;
+            }
+        }
+
+        return outputArray;
+    }
+}
